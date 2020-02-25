388cd3ecea421d3f5f3de8559be705d6c0588b95
przemekSzafert <szafert.p@gmail.com>
2020-02-25 20:27:43 +0100

    mid section 8

--- client/src/queries.js
+++ client/src/queries.js
@@ -14,9 +14,42 @@ export const GET_POSTS = gql`
 
 
 /* User Queries */
-
+export const GET_CURRENT_USER = gql`
+  query {
+    getCurrentUser{
+      _id
+      username
+      email
+      password
+      avatar
+      joinDate
+      favorites{
+        _id
+        title
+        imageUrl
+      }
+    }
+  }
+`
 
 /* Posts Mutations */
 
 
 /* User Mutations */
+
+export const SIGNIN_USER = gql`
+mutation($username:String!, $password: String!){
+  signinUser(username:$username, password:$password){
+    token
+  }
+}
+
+`;
+
+export const SIGNUP_USER = gql`
+mutation($username:String!, $email:String!, $password:String!) {
+  signupUser(username: $username, email:$email, password:$password){
+  token
+  }
+}
+`;
