8f5b8b599696249109ac002e460ecab8e52a5fdb
przemekSzafert <szafert.p@gmail.com>
2020-02-25 17:44:51 +0100

    section 7 completed

--- client/src/main.js
+++ client/src/main.js
@@ -1,27 +1,27 @@
-import '@babel/polyfill'
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import "@babel/polyfill";
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-
-import ApolloClient from 'apollo-boost';
-import VueApollo from 'vue-apollo';
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
 
 Vue.use(VueApollo);
 
-const defaultClient = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
+// Setup ApolloClient
+export const defaultClient = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
 });
 
 const apolloProvider = new VueApollo({ defaultClient });
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   apolloProvider,
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
